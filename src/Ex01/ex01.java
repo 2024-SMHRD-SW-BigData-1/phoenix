6c2cb6bde2e3ffcfcca527ad4d8e90759cc8e604
김형준 <hyeongjun1125@gmail.com>
2024-04-15 09:44:32 +0900

    1st commit

--- src/Ex01/ex01.java
+++ src/Ex01/ex01.java
@@ -3,5 +3,16 @@ package Ex01;
 public class ex01 {
 public static void main(String[] args) {
 	System.out.println("개인");
+	System.out.println("개인");
+	System.out.println("개인");
+	System.out.println("개인");
+	System.out.println("개인");
+	System.out.println("개인");
+	System.out.println("개인");
+	System.out.println("개인");
+	System.out.println("개인");
+	System.out.println("개인");
+	System.out.println("개인");
+	
 }
 }
