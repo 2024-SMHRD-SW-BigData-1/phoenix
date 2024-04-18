34ed0b906ae850dbdca44db587ffaa51daeb0bff
김형준 <91448039+xm4hy@users.noreply.github.com>
2024-04-18 15:34:16 +0900

    Update DAO.java

--- src/Model/DAO.java
+++ src/Model/DAO.java
@@ -18,16 +18,16 @@ public class DAO {
 
       ArrayList<musicvo> Drama_list = new ArrayList<>();
 
-      Drama_list.add(new musicvo("눈물의 여왕", "C:/Users/smhrd/Desktop/mini_music_drama/부석순 자꾸만 웃게 돼 눈물의여왕.mp3"));
-      Drama_list.add(new musicvo("신사와 아가씨", "C:/Users/smhrd/Desktop/mini_music_drama/임영웅 사랑은 늘 도망가 신사와 아가씨.mp3"));
-      Drama_list.add(new musicvo("도깨비", "C:/Users/smhrd/Desktop/mini_music_drama/에일리 첫눈처럼 너에게 가겠다 도깨비.mp3"));
-      Drama_list.add(new musicvo("아내의 유혹", "C:/Users/smhrd/Desktop/mini_music_drama/차수경 용서 못해 아내의유혹.mp3"));
-      Drama_list.add(new musicvo("시크릿가든", "C:/Users/smhrd/Desktop/mini_music_drama/김범수 나타나 시크릿 가든.mp3"));
-      Drama_list.add(new musicvo("스카이캐슬", "C:/Users/smhrd/Desktop/mini_music_drama/하진 We All Lie We all lie 스카이캐슬.mp3"));
-      Drama_list.add(new musicvo("미생", "C:/Users/smhrd/Desktop/mini_music_drama/한희정 Tomorrow 미생.mp3"));
-      Drama_list.add(new musicvo("시그널", "C:/Users/smhrd/Desktop/mini_music_drama/정차식 나는 너를 I will forget you 시그널.mp3"));
-      Drama_list.add(new musicvo("미안하다 사랑하다", "C:/Users/smhrd/Desktop/mini_music_drama/박효신 눈의 꽃 미안하다사랑한다.mp3"));
-      Drama_list.add(new musicvo("연애시대", "C:/Users/smhrd/Desktop/mini_music_drama/스윗소로우 아무리 생각해도 난 너를 연애시대.mp3"));
+      Drama_list.add(new musicvo("눈물의 여왕", "C:/Users/smhrd1/Desktop/mini_music_drama/부석순 자꾸만 웃게 돼 눈물의여왕.mp3"));
+      Drama_list.add(new musicvo("신사와 아가씨", "C:/Users/smhrd1/Desktop/mini_music_drama/임영웅 사랑은 늘 도망가 신사와 아가씨.mp3"));
+      Drama_list.add(new musicvo("도깨비", "C:/Users/smhrd1/Desktop/mini_music_drama/에일리 첫눈처럼 너에게 가겠다 도깨비.mp3"));
+      Drama_list.add(new musicvo("아내의 유혹", "C:/Users/smhrd1/Desktop/mini_music_drama/차수경 용서 못해 아내의유혹.mp3"));
+      Drama_list.add(new musicvo("시크릿가든", "C:/Users/smhrd1/Desktop/mini_music_drama/김범수 나타나 시크릿 가든.mp3"));
+      Drama_list.add(new musicvo("스카이캐슬", "C:/Users/smhrd1/Desktop/mini_music_drama/하진 We All Lie We all lie 스카이캐슬.mp3"));
+      Drama_list.add(new musicvo("미생", "C:/Users/smhrd1/Desktop/mini_music_drama/한희정 Tomorrow 미생.mp3"));
+      Drama_list.add(new musicvo("시그널", "C:/Users/smhrd1/Desktop/mini_music_drama/정차식 나는 너를 I will forget you 시그널.mp3"));
+      Drama_list.add(new musicvo("미안하다 사랑하다", "C:/Users/smhrd1/Desktop/mini_music_drama/박효신 눈의 꽃 미안하다사랑한다.mp3"));
+      Drama_list.add(new musicvo("연애시대", "C:/Users/smhrd1/Desktop/mini_music_drama/스윗소로우 아무리 생각해도 난 너를 연애시대.mp3"));
 
       return Drama_list;
    }
@@ -36,16 +36,16 @@ public class DAO {
 
       ArrayList<musicvo> Drama_list = new ArrayList<>();
 
-      Drama_list.add(new musicvo("클래식","C:/Users/smhrd/Desktop/mini_music_drama/자전거탄풍경 너에게 난 나에게 넌 Me to You You to Me 클래식.mp3"));
-      Drama_list.add(new musicvo("겨울왕국", "C:/Users/smhrd/Desktop/mini_music_drama/Idina Menzel 이디나 멘젤 Let It Go 겨울왕국.mp3"));
-      Drama_list.add(new musicvo("타이타닉","C:/Users/smhrd/Desktop/mini_music_drama/Céline Dion My Heart Will Go On 타이타닉.mp3"));
-      Drama_list.add(new musicvo("웰컴 투 동막골", "C:/Users/smhrd/Desktop/mini_music_drama/A Waltz of Sleigh 웰컴 투 동막골.mp3"));
-      Drama_list.add(new musicvo("비긴어게인","C:/Users/smhrd/Desktop/mini_music_drama/너 같은 사람이 필요해 Adam Levine No One Else Like You 비긴어게인.mp3"));
-      Drama_list.add(new musicvo("범죄와의 전쟁", "C:/Users/smhrd/Desktop/mini_music_drama/장기하와 얼굴들 풍문으로 들었소 범죄와의 전쟁.mp3"));
-      Drama_list.add(new musicvo("몬스터 주식회사", "C:/Users/smhrd/Desktop/mini_music_drama/If I Didn t Have You 몬스터 주식회사.mp3"));
-      Drama_list.add(new musicvo("살인의 추억", "C:/Users/smhrd/Desktop/mini_music_drama/우울한 편지 유재하 살인의 추억.mp3"));
-      Drama_list.add(new musicvo("과속스캔들", "C:/Users/smhrd/Desktop/mini_music_drama/박보영 아마도 그건 과속스캔틀OST.mp3"));
-      Drama_list.add(new musicvo("하울의 움직이는 성", "C:/Users/smhrd/Desktop/mini_music_drama/인생의 회전목마 하울의 움직이는 성 OST.mp3"));
+      Drama_list.add(new musicvo("클래식","C:/Users/smhrd1/Desktop/mini_music_drama/자전거탄풍경 너에게 난 나에게 넌 Me to You You to Me 클래식.mp3"));
+      Drama_list.add(new musicvo("겨울왕국", "C:/Users/smhrd1/Desktop/mini_music_drama/Idina Menzel 이디나 멘젤 Let It Go 겨울왕국.mp3"));
+      Drama_list.add(new musicvo("타이타닉","C:/Users/smhrd1/Desktop/mini_music_drama/Céline Dion My Heart Will Go On 타이타닉.mp3"));
+      Drama_list.add(new musicvo("웰컴 투 동막골", "C:/Users/smhrd1/Desktop/mini_music_drama/A Waltz of Sleigh 웰컴 투 동막골.mp3"));
+      Drama_list.add(new musicvo("비긴어게인","C:/Users/smhrd1/Desktop/mini_music_drama/너 같은 사람이 필요해 Adam Levine No One Else Like You 비긴어게인.mp3"));
+      Drama_list.add(new musicvo("범죄와의 전쟁", "C:/Users/smhrd1/Desktop/mini_music_drama/장기하와 얼굴들 풍문으로 들었소 범죄와의 전쟁.mp3"));
+      Drama_list.add(new musicvo("몬스터 주식회사", "C:/Users/smhrd1/Desktop/mini_music_drama/If I Didn t Have You 몬스터 주식회사.mp3"));
+      Drama_list.add(new musicvo("살인의 추억", "C:/Users/smhrd1/Desktop/mini_music_drama/우울한 편지 유재하 살인의 추억.mp3"));
+      Drama_list.add(new musicvo("과속스캔들", "C:/Users/smhrd1/Desktop/mini_music_drama/박보영 아마도 그건 과속스캔틀OST.mp3"));
+      Drama_list.add(new musicvo("하울의 움직이는 성", "C:/Users/smhrd1/Desktop/mini_music_drama/인생의 회전목마 하울의 움직이는 성 OST.mp3"));
 
       return Drama_list;
    }
